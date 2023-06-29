7c3f3bd852400e4ebadf968b1589d4580f42235c
Ashik Ghosh <ashikghosh763@gmail.com>
2023-06-30 01:59:48 +0600

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+// export const mongoURI = {'mongodb+srv://ashik:ashik@cluster0.he6kc5b.mongodb.net/'}
+
+module.exports ={
+    mongoURI :'mongodb+srv://ashik:ashik@cluster0.he6kc5b.mongodb.net/'
+};
