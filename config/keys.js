f4e1acc153ada87d3e54149d73bc3a4af01985fc
Ashik Ghosh <ashikghosh763@gmail.com>
2023-09-20 23:32:30 +0600

    fixed some things

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-// export const mongoURI = {'mongodb+srv://ashik:ashik@cluster0.he6kc5b.mongodb.net/'}
-
+console.log(process.env.SECRET_OR_KEY);
 module.exports ={
-    mongoURI :'mongodb+srv://ashik:ashik@cluster0.he6kc5b.mongodb.net/',
-    secretOrKey: 'secret'
+    mongoURI :`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.he6kc5b.mongodb.net/`,
+    secretOrKey: `${process.env.SECRET_OR_KEY}`
 };
--- server.js
+++ server.js
@@ -3,38 +3,40 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
-
+require('dotenv').config();
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
 
-// Body parser middleware
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB Config
+
 const db = require('./config/keys').mongoURI;
 
-// Connect to MongoDB
+
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-// Passport middleware
+
 app.use(passport.initialize());
 
-// Passport Config
-require('./config/passport')(passport);
 
-// Use Routes
+require('./config/passport')(passport);
+app.get('/',(req, res) => {
+    res.json("HELLO");
+})
+
+
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-// Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
