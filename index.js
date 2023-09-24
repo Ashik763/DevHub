b6b494b68f5c7f1a5c5b623b9e04730fbe41e50d
Ashik Ghosh <ashikghosh763@gmail.com>
2023-09-24 21:12:16 +0600

    done

--- index.js
+++ index.js
@@ -37,14 +37,14 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 // "proxy": "https://dev-hub-backend.vercel.app//"
 
 
